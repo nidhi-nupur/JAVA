b256d9715f0b7a9981f2b89657dca0d56b101440
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-04-23 11:33:33 +0530

    Started learning function

---
+++ Function/printHW.java
@@ -0,0 +1,11 @@
+public class printHW {
+    public static void printHelloWorld() {
+        System.out.println("Hello World"); //Methods :
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return;
+    }
+    public static void main(String args[]) {
+        printHelloWorld();//Function Calling
+    }
+}
