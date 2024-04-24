5d98e3ed8a674031c26722db0114023b45afc51d
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-04-24 10:13:35 +0530

    without function

---
+++ Function/SwapDigit.java
@@ -0,0 +1,11 @@
+public class SwapDigit {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
