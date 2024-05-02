a1cd6a57ca306f462c620f6aaa6510344f4e9b13
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-05-02 10:26:47 +0530

    My way

---
+++ Function/avgOfThreeNum.java
@@ -0,0 +1,9 @@
+public class avgOfThreeNum {
+    public static void avgValue(int a, int b, int c) {
+        float avg = (float)(a + b + c)/3;
+        System.out.println(avg);
+    }
+    public static void main(String[] args) {
+        avgValue(4, 6, 7);
+    }
+}
