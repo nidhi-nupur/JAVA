c981257efbff24ba61ce0af404dfb25b3ea81f7d
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-05-08 11:53:50 +0530

    Add files via upload

---
+++ Array/arrayCreation.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class arrayCreation {
+    public static void main(String[] args) {
+        int arrayName[] = new int[50]; // size of array can't be change at run time.
+        int numbers[] = {1,2,3,4}; // size of array will automatically determined by the numbers of array elts. (size of array = 4)
+        int moreNumbers[] = {5,7,3}; // (size of array = 3)
+        String friends[] = {"xyz","abc","qwe","sdfg","fghkli"}; // (size of array = 5)  
+    }
+}
