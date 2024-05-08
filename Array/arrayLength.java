53c1e0ad255cb5ee35e7495f45c86a3aca2204b2
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-05-08 11:55:37 +0530

    Using in-built function

---
+++ Array/arrayLength.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class arrayLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arrayA[] = new int[294];
+        int arrayB[] = new int[34];
+        int arrayC[] = new int[100];
+        int arrayD[] = new int[57];
+        System.out.println("Size of ArrayA: " + arrayA.length);
+        System.out.println("Size of ArrayB: " + arrayB.length);
+        System.out.println("Size of ArrayC: " + arrayC.length);
+        System.out.println("Size of ArrayD: " + arrayD.length);
+    }
+}
