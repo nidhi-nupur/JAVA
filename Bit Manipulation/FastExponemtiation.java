f27fd7d919871e48384b209c187ff75631ec5130
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-06-13 20:00:37 +0530

    Update FastExponemtiation.java
    It consists two codes, 2nd one is understanding the first one.

--- Bit Manipulation/FastExponemtiation.java
+++ Bit Manipulation/FastExponemtiation.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class FastExponemtiation {
+    
+    
+    // Tutorial's code.
     public static int fastExponent(int number, int a) {
         int answer = 1;
         while (number > 0) {
@@ -19,5 +22,35 @@ public class FastExponemtiation {
         System.out.println("Enter the power of that number: ");
         int power = sc.nextInt();
         System.out.println("The number " + number + " raise to power " + power + " is " + fastExponent(power, number));
+    }
+
+
+
+
+    
+
+    // For better understanding: Crystal Clear Code.
+    public static int fastExponent(int base, int exponent) {
+        int result = 1; // Initialize result.
+        while(exponent > 0) {
+            // If the current LSB is set("1"), multiply it with the result.
+            // If the next bit is non-set("0"), skip multiplication.
+            if ((exponent & 1) == 1) {
+                result = result * base;
+            }
+            // Square the base
+            base = base * base;
+            // Right shift the exponent
+            exponent = exponent >> 1;
+        }
+        return result; // Return the final result.
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Base: ");
+        int base = sc.nextInt(); // Base Number.
+        System.out.println("Exponent: ");
+        int exponent = sc.nextInt(); // Exponent.
+        System.out.println("Result of " + base + "^" + exponent + " is: " + fastExponent(base, exponent));
     }
 }
