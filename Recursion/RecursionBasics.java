2994c3b2c96d9f4b0c048df0e4dca5e4ab0d1182
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-06-24 20:56:11 +0530

    Update RecursionBasics.java
    Code to Print numbers:
    
    - in Decreasing Order from N to 1.
    - in Increasing Order from 1 to N.

--- Recursion/RecursionBasics.java
+++ Recursion/RecursionBasics.java
@@ -16,12 +16,24 @@ public class RecursionBasics {
         printDecreasingOrder(number - 1); // Recursive case.
     }
 
+
+    // Print the number in Increasing Order from 1 to n.
+    public static void printIncreasingOrder(int number) {
+        // Base case
+        if(number == 1) {
+            System.out.print(number);
+            return;
+        }
+        printIncreasingOrder(number - 1); // Recursive case.
+        System.out.print( " " + number);
+    }
+
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
         printDecreasingOrder(num);
+        printIncreasingOrder(num); 
     }
-
-
-    // Print the number in Increasing Order from 1 to n.
+    
 }
