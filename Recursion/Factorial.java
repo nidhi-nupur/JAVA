f08608672b5d9ac6c598c27a8135491a1ea32dc0
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-06-25 14:35:27 +0530

    Add files via upload

---
+++ Recursion/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static int calculateFactorial(int number) {
+        // Base case
+        if(number == 0) {
+            return 1;
+        }
+        int factNminus1 = calculateFactorial(number - 1);
+        int fact = number * calculateFactorial(number - 1);
+        return fact;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        System.out.println(calculateFactorial(num));
+    }
+}
