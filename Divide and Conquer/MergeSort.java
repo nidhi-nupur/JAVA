92c4fb771d500ded2ece9293c652c5362657f1f4
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-07-26 17:50:29 +0530

    Update MergeSort.java

--- Divide and Conquer/MergeSort.java
+++ Divide and Conquer/MergeSort.java
@@ -4,7 +4,7 @@ public class MergeSort {
 
 // Depth first Merge Sort
     // TC = O(nlogn)
-    // SC = O(n)
+    // SC = O(n) --> Size of temperory array.
     public static void printArray(int array[]) {
         System.out.println("The Sorted array is: ");
         for(int i = 0; i < array.length; i++) {
@@ -13,10 +13,27 @@ public class MergeSort {
         System.out.println();
     }
 
+    public static void mergeSorting(int array[], int si, int ei) {
+        // Base Case:
+        if(si >= ei) {
+            return; // When the si and ei are equal, the array is already sorted cause it is single element array.
+        }
+        // Main work:
+        // Dividing the array:
+        int mid = si + (ei - si)/2; // This method Avoids Integer overflow. // Other way: (si + ei)/2.
+
+        // Sorting the divided part of the array.
+        mergeSorting(array, si, mid); // Left part
+        mergeSorting(array, mid + 1, ei); // Right part
+
+        // Merging the sorted part of the divided array.
+        merge(array, si, mid, ei); // Calling the function having temperory array.
+    }
+
     public static void merge(int array[], int si, int mid, int ei) {
         // left(0, 3)=4 elts   right(4, 6)=3 elts 
         // --> 6(ei) - 0(si) + 1 == 7 elts
-        int temp[] = new int[ei - si + 1];
+        int temp[] = new int[ei - si + 1]; // Let the size is 5, means si = 0 & ei = 4, [ei(4) - si(0) = 4(size)] but the size is 5, that is why [ei(4) - si(0) + 1 = 5(size)].
         int i = si; // iterator for left part.
         int j = mid + 1; // iterator for right part.
         int k = 0; // iterator for temperory array.
@@ -33,6 +50,19 @@ public class MergeSort {
                 k++;
             }
         }
+        // OR
+        /*
+        while(i <= mid && j <= ei) {
+            if(array[i] < array[j]) {
+                temp[k] = array[i];
+                i++;
+            } else {
+                temp[k] = array[j];
+                j++;
+            }
+            k++;
+        }
+        */
 
         // For copying all elements from the 2nd part of the divided part of the original array.
         // Left Part
@@ -51,22 +81,7 @@ public class MergeSort {
         }
     }
 
-    public static void mergeSorting(int array[], int si, int ei) {
-        // Base Case:
-        if(si >= ei) {
-            return; // When the si and ei are equal, the array is already sorted cause it is single element array.
-        }
-        // Main work:
-        // Dividing the array:
-        int mid = si + (ei - si)/2;
-
-        // Sorting the divided part of the array.
-        mergeSorting(array, si, mid); // Left part
-        mergeSorting(array, mid + 1, ei); // Right part
-
-        // Merging the sorted part of the divided array.
-        merge(array, si, mid, ei); // Calling the function having temperory array.
-    }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Size of the array: ");
