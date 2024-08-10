969923dcbe0a28323572775d8514341de8c67c83
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-08-10 17:21:21 +0530

    Add files via upload

---
+++ ArrayLists/Initialize.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Initialize {
+
+    public static void main(String[] args) {
+        // Syntax:-
+        // ClassName ObjectName = new Classname<>();
+        ArrayList<Integer> nidhi= new ArrayList<>();
+        ArrayList<String> name = new ArrayList<>();
+        ArrayList<Boolean> objName = new ArrayList<>();
+        System.out.println(name);
+        System.out.println(nidhi);
+        System.out.println(objName);
+    }
+}
