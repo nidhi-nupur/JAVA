2bff19a00d9c53c48d8036dea6ab5097a81bafd5
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-09-02 01:56:58 +0530

    Add files via upload

---
+++ ArrayLists/PairSum2_BruteForce.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class PairSum2_BruteForce {
+    // Function to check if there exists a pair with the given target sum
+    public static boolean PairSum2 (ArrayList<Integer> list, int target) {
+        // Check each pair in the array
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                // If a pair with the target sum is found
+                if (list.get(i) + list.get(j) == target) {
+                    return true;
+                }
+            }
+        }
+        // If no such pair is found
+        return false;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        
+        if (PairSum2(list, 16)) {
+            System.out.println("Pair with given target sum exists.");
+        } else {
+            System.out.println("No Pair with given target sum exists.");
+        }
+        if (PairSum2(list, 1)) {
+            System.out.println("Pair with given target sum exists.");
+        } else {
+            System.out.println("No Pair with given target sum exists.");
+        }
+        if (PairSum2(list, 30)) {
+            System.out.println("Pair with given target sum exists.");
+        } else {
+            System.out.println("No Pair with given target sum exists.");
+        }
+        if (PairSum2(list, 7)) {
+            System.out.println("Pair with given target sum exists.");
+        } else {
+            System.out.println("No Pair with given target sum exists.");
+        }
+    }
+}
