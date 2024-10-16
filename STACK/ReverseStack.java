bb7268ed95675d702e863ec07c1dae44a5a2decb
Nidhi Nupur <154660722+nidhi-nupur@users.noreply.github.com>
2024-10-16 18:44:09 +0530

    Add files via upload

---
+++ STACK/ReverseStack.java
@@ -0,0 +1,55 @@
+// DSA Question - AMAZON, FlipKart, Adobe, Microsoft, Paytm.
+// Question 2 : Reverse a String using Stack.
+
+import java.util.Stack;
+
+public class ReverseStack {
+
+    public static void reverseString1(String string) {
+        Stack<Character> stack = new Stack<>();
+
+        int index = 0;
+
+        while(index < string.length()) {
+            stack.push(string.charAt(index));
+            index++;
+        }
+
+        StringBuilder result = new StringBuilder(""); // to store the results in the StringBuilder from the stack.
+        while(!stack.isEmpty()) {
+            char current = stack.pop(); // deleting the char of the string from the stack.
+            result.append(current); // adding the char of the string from the stack to the StringBuilder [result].
+        }
+        string = result.toString(); // Assigning the resultant string to the String from the StringBuilder.
+
+    }
+
+
+    public static String reverseString2(String string) {
+        Stack<Character> stack = new Stack<>();
+
+        int index = 0;
+
+        while(index < string.length()) {
+            stack.push(string.charAt(index));
+            index++;
+        }
+
+        StringBuilder result = new StringBuilder(""); // to store the results in the StringBuilder from the stack.
+        while(!stack.isEmpty()) {
+            char current = stack.pop(); // deleting the char of the string from the stack.
+            result.append(current); // adding the char of the string from the stack to the StringBuilder [result].
+        }
+        return result.toString(); // Assigning the resultant string to the String from the StringBuilder.
+
+    }
+    public static void main(String[] args) {
+        String string = "abc";
+        String result2 = reverseString2(string);
+        System.out.println(result2);
+       // String str2 = "lmn";
+       // String result1 = reverseString1(str2);
+       // System.out.println(result1);
+
+    }
+}
